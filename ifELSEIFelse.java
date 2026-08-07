7fa5126f609384f18783aa98785f72cdc4a4e570
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-07 12:25:33 +0530

    Add files via upload

---
+++ ifELSEIFelse.java
@@ -0,0 +1,24 @@
+package CoreJava;
+
+public class ifELSEIFelse {
+
+	public static void main(String[] args) {
+		int age=75;
+		if(age<=3)
+		{
+			System.out.println("Child");
+		}else if(age>=4 && age<=12)
+		{
+			System.out.println("Kid");
+			
+		}else if(age>=13 && age <=18)
+		{
+			System.out.println("Teenager");
+		}else if(age>=19 && age<=60)
+		{
+			System.out.println("Adult");
+		}else {
+			System.out.println("Old Age");
+		}
+	}
+}
