16e2fb0a9060fe2aec3f89f4b4a965bd51ef95a7
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-08 10:17:11 +0530

    Add files via upload

---
+++ FORLoop.java
@@ -0,0 +1,14 @@
+package CoreJava;
+import java.util.Scanner;
+public class FORLoop {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num=100;
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println("Ewans Angel LS");
+		}
+	}
+
+}
