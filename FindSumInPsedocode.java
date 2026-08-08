b28fceba0be7947f5d8bad337df6c1b2553c922a
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-08 11:17:43 +0530

    Add files via upload

---
+++ FindSumInPsedocode.java
@@ -0,0 +1,15 @@
+package CoreJava;
+import java.util.Scanner;
+public class FindSumInPsedocode {
+
+	public static void main(String[] args) {
+		int num=5;
+		int sum=0;
+		for(int i=1;i<=5;i++) {
+			sum=sum+i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
