0c5d4cc3c52d0d3b9fc2efe71f02ad4eb904c084
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-10 09:35:55 +0530

    Add files via upload

---
+++ HelpPenny.java
@@ -0,0 +1,15 @@
+package CoreJava;
+import java.util.Scanner;
+public class HelpPenny {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		for(int i=1;i<5;i++)
+		{
+			System.out.println(i+"*"+m+"="+1*m);
+		}
+	}
+
+}
