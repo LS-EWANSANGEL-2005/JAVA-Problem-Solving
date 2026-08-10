838a2bcc01da36d09518a34116392c32d429e794
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-10 09:27:32 +0530

    Add files via upload

---
+++ DoWhileWHILELoop.java
@@ -0,0 +1,14 @@
+package CoreJava;
+
+public class DoWhileWHILELoop {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+
+	}
+
+}
